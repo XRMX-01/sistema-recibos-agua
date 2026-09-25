1915b71d32c585f61d46f839f88d318dbf4723b3
XRMX-01 <cleiverpcc5@gmail.com>
2026-09-25 15:56:08 -0500

    Refactor web server code for clarity and structure

--- servidor_web.py
+++ servidor_web.py
@@ -129,6 +129,4 @@ def logout():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)main__':
-    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
